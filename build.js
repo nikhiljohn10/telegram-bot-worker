f0a7ad7054fbf8f6c89cf8fa2b1230623c69ec91
Sean Behan <codebam@riseup.net>
2022-02-17 11:09:07 -0500

    update version, remove esbuild catch

--- build.js
+++ build.js
@@ -1,8 +1,6 @@
-require("esbuild")
-  .build({
-    entryPoints: ["src/worker.ts"],
-    format: "esm",
-    bundle: true,
-    outfile: "dist/worker.mjs",
-  })
-  .catch(() => process.exit(1));
+require("esbuild").build({
+  entryPoints: ["src/worker.ts"],
+  format: "esm",
+  bundle: true,
+  outfile: "dist/worker.mjs",
+});
