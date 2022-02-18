f65e8aa84fe563bfe2a2bdd5666574a80dae6324
Sean Behan <codebam@riseup.net>
2022-02-18 04:10:37 -0500

    remove unneeded async keyword

--- src/libs.ts
+++ src/libs.ts
@@ -10,7 +10,7 @@ export function JSONResponse(data, status = 200) {
 }
 
 // Generate InlineQueryResultArticle
-export const InlineQueryResultArticle = async (content, parse_mode = "") =>
+export const InlineQueryResultArticle = (content, parse_mode = "") =>
   sha256(content).then((id) => ({
     type: "article",
     id: id.toString(),
