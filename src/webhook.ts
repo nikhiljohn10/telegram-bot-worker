a2d866ded49e36cab7fdfd5a511128d6d8c70a07
Sean Behan <codebam@riseup.net>
2022-02-20 00:49:37 -0500

    rewrite webhook

--- src/webhook.ts
+++ src/webhook.ts
@@ -14,55 +14,32 @@ export default class Webhook {
   // trigger getMe command of BotAPI
   getMe = () => this.execute(this.api + "/getMe");
 
-  set = async () => {
-    const access_key = sha256(this.token.toString());
-    const max_connections = 100;
-    const allowed_updates = ["message"];
-    return this.execute(
-      this.api +
-        `/setWebhook?url=${encodeURIComponent(
-          this.url + access_key
-        )}&max_connections=${max_connections}&allowed_updates=${allowed_updates}&drop_pending_updates=True`
+  set = async () =>
+    this.execute(
+      `${this.api}/setWebhook?url=${encodeURIComponent(
+        `${this.url}${sha256(this.token).toString()}`
+      )}&max_connections=${100}&allowed_updates=${[
+        "message",
+      ]}&drop_pending_updates=${true}`
     );
-  };
 
-  get = async () => this.execute(this.api + "/getWebhookInfo");
+  get = async () => this.execute(`${this.api}/getWebhookInfo`);
 
-  delete = async () => this.execute(this.api + "/deleteWebhook");
+  delete = async () => this.execute(`${this.api}/deleteWebhook`);
 
   execute = async (url) =>
     fetch(url)
       .then((response) => response.json())
       .then((json) => JSONResponse(json));
 
-  process = async (url) => {
-    const command = url.searchParams.get("command");
-    if (command == undefined) {
-      return this.error("No command found", 404);
-    }
-    // handles the url commands
-    switch (command) {
-      case "setWebhook":
-        return await this.set();
-      case "getWebhook":
-        return await this.get();
-      case "delWebhook":
-        return await this.delete();
-      case "getMe":
-        return await this.getMe();
-      case "":
-        return this.error("No command found", 404);
-      default:
-        return this.error("Invalid command", 400);
-    }
+  webhookCommands = {
+    setWebhook: this.set,
+    getWebhook: this.get,
+    delWebhook: this.delete,
+    getMe: this.getMe,
+    default: JSONResponse({ error: "Invalid command" }, 400),
   };
 
-  // handles error responses
-  error = (message, status = 403) =>
-    JSONResponse(
-      {
-        error: message,
-      },
-      status
-    );
+  process = async (url) =>
+    this.webhookCommands[url.searchParams.get("command")]();
 }
