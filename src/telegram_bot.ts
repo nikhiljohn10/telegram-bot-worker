fb1be95b9f6893d8e694992d7abba8ea098d1420
Sean Behan <codebam@riseup.net>
2022-02-22 04:26:03 -0500

    uncomment recursion, it only sends about 20 messages because of the subrequest limit

--- src/telegram_bot.ts
+++ src/telegram_bot.ts
@@ -248,26 +248,27 @@ export default class TelegramBot extends Bot {
 
   // bot command: /recursion
   recursion = async (update): Promise<Response> =>
-    // this.sendMessage(update.message.chat.id, "/recursion").then((response) =>
-    //   response
-    //     .clone()
-    //     .json()
-    //     .then((result: { ok: boolean; result: { text: string } }) =>
-    //       this.handler.postResponse(
-    //         new Request("", {
-    //           method: "POST",
-    //           body: JSON.stringify({
-    //             message: {
-    //               text: result.result.text,
-    //               chat: { id: update.message.chat.id },
-    //             },
-    //           }),
-    //         }),
-    //         this
-    //       )
-    //     )
-    // );
-    new Response();
+    this.sendMessage(update.message.chat.id, "/recursion")
+      .then((response) =>
+        response
+          .clone()
+          .json()
+          .then((result: { ok: boolean; result: { text: string } }) =>
+            this.handler.postResponse(
+              new Request("", {
+                method: "POST",
+                body: JSON.stringify({
+                  message: {
+                    text: result.result.text,
+                    chat: { id: update.message.chat.id },
+                  },
+                }),
+              }),
+              this
+            )
+          )
+      )
+      .catch(() => new Response());
 
   // bot command: /roll
   roll = async (update, args) =>
