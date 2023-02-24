920fffc91821728bba53bb4ebe795003d4bd5fef
Sean Behan <codebam@riseup.net>
2023-02-23 22:00:42 -0500

    remove double slash in webhook url

--- packages/main/src/telegram_webhook.ts
+++ packages/main/src/telegram_webhook.ts
@@ -11,7 +11,10 @@ export default class TelegramWebhook extends Webhook {
 		sha256(this.token).then((access_key) =>
 			fetch_json(
 				addSearchParams(new URL(`${this.api.href}/setWebhook`), {
-					url: new URL(`${this.url.href}/${access_key}`).href,
+					url: new URL(`${this.url.href}/${access_key}`).href.replace(
+						/([^:]\/)\/+/g,
+						"$1"
+					),
 					max_connections: "100",
 					allowed_updates: JSON.stringify(["message", "inline_query"]),
 					drop_pending_updates: drop_pending_updates.toString(),
