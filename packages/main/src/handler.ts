80250783c8901c1d8c1a2925e3d39f6bcfedbfc8
Sean Behan <codebam@riseup.net>
2023-08-16 15:32:29 -0400

    try to remove any

--- packages/main/src/handler.ts
+++ packages/main/src/handler.ts
@@ -3,83 +3,83 @@ import { sha256, log } from "./libs";
 import { Config, Update, localhost } from "./types";
 
 export default class Handler {
-  configs: Partial<Config>[];
+	configs: Partial<Config>[];
 
-  constructor(configs: Partial<Config>[]) {
-    this.configs = configs;
-  }
+	constructor(configs: Partial<Config>[]) {
+		this.configs = configs;
+	}
 
-  getResponse = async (
-    _request?: Request,
-    _bot?: BotApi
-  ): Promise<Response> => {
-    this.getAccessKeys(this.configs).then((access_keys) =>
-      Object.keys(access_keys).forEach((key) =>
-        log(
-          `${access_keys[key].bot_name} ${((request_url) =>
-            `${request_url.origin}${request_url.pathname}`)(
-            new URL(_request?.url ?? localhost)
-          )}${key}`
-        )
-      )
-    );
-    if (_bot?.webhook.token) {
-      return _bot.webhook.process(new URL(_request?.url ?? localhost));
-    }
-    return this.responses.default();
-  };
+	getResponse = async (
+		_request?: Request,
+		_bot?: BotApi
+	): Promise<Response> => {
+		this.getAccessKeys(this.configs).then((access_keys) =>
+			Object.keys(access_keys).forEach((key) =>
+				log(
+					`${access_keys[key].bot_name} ${((request_url) =>
+						`${request_url.origin}${request_url.pathname}`)(
+						new URL(_request?.url ?? localhost)
+					)}${key}`
+				)
+			)
+		);
+		if (_bot?.webhook.token) {
+			return _bot.webhook.process(new URL(_request?.url ?? localhost));
+		}
+		return this.responses.default();
+	};
 
-  postResponse = async (
-    _request?: Request,
-    _bot?: BotApi
-  ): Promise<Response> =>
-    _bot?.webhook.token === ""
-      ? this.responses.default()
-      : _request
-      ? _request
-          .json()
-          .then((update) => (_bot as BotApi).update(update as Update))
-      : this.responses.default();
+	postResponse = async (
+		_request?: Request,
+		_bot?: BotApi
+	): Promise<Response> =>
+		_bot?.webhook.token === ""
+			? this.responses.default()
+			: _request
+			? _request
+					.json()
+					.then((update) => (_bot as BotApi).update(update as Update))
+			: this.responses.default();
 
-  responses: Record<
-    string,
-    (_request?: Request, _bot?: BotApi) => Promise<Response>
-  > = {
-    GET: this.getResponse,
-    POST: this.postResponse,
-    default: () => new Promise(() => new Response()),
-  };
+	responses: Record<
+		string,
+		(_request?: Request, _bot?: BotApi) => Promise<Response>
+	> = {
+		GET: this.getResponse,
+		POST: this.postResponse,
+		default: () => new Promise(() => new Response()),
+	};
 
-  getAccessKeys = async (
-    configs: Partial<Config>[]
-  ): Promise<Record<string, Config> | Record<string, never>> =>
-    Promise.all(
-      configs.map((bot_config: Partial<Config>) =>
-        sha256(bot_config.webhook?.token ?? "").then((hash) => [
-          hash,
-          bot_config,
-        ])
-      )
-    ).then((result) => Object.fromEntries(result));
+	getAccessKeys = async (
+		configs: Partial<Config>[]
+	): Promise<Record<string, Config> | Record<string, never>> =>
+		Promise.all(
+			configs.map((bot_config: Partial<Config>) =>
+				sha256(bot_config.webhook?.token ?? "").then((hash) => [
+					hash,
+					bot_config,
+				])
+			)
+		).then((result) => Object.fromEntries(result));
 
-  // handles the request
-  handle = async (request: Request): Promise<Response> =>
-    this.getAccessKeys(this.configs).then((access_keys) =>
-      Object.keys(this.responses).includes(request.method)
-        ? this.responses[request.method](
-            request,
-            ((key) => {
-              if (access_keys[key]) {
-                return new access_keys[key].api({
-                  ...new Config(),
-                  ...access_keys[key],
-                  url: new URL(new URL(request.url).origin), // worker url
-                  handler: this,
-                });
-              }
-              return this.responses.default();
-            })(new URL(request.url).pathname.substring(1))
-          )
-        : this.responses.default()
-    );
+	// handles the request
+	handle = async (request: Request): Promise<Response> =>
+		this.getAccessKeys(this.configs).then((access_keys) =>
+			Object.keys(this.responses).includes(request.method)
+				? this.responses[request.method](
+						request,
+						((key) => {
+							if (access_keys[key]) {
+								return access_keys[key].api({
+									...new Config(),
+									...access_keys[key],
+									url: new URL(new URL(request.url).origin), // worker url
+									handler: this,
+								});
+							}
+							return this.responses.default();
+						})(new URL(request.url).pathname.substring(1))
+				  )
+				: this.responses.default()
+		);
 }
--- packages/main/src/types.ts
+++ packages/main/src/types.ts
@@ -2,18 +2,19 @@ import BotApi from "./bot_api";
 import Handler from "./handler";
 import TelegramApi from "./telegram_api";
 import Webhook from "./webhook";
+import TelegramWebhook from "./telegram_webhook";
 export { Webhook };
 
 export type Command = (
-  bot: BotApi,
-  update: Update,
-  args?: string[]
+	bot: BotApi,
+	update: Update,
+	args?: string[]
 ) => Promise<Response>;
 
 export type TelegramCommand = (
-  bot: TelegramApi,
-  update: TelegramUpdate,
-  args?: string[]
+	bot: TelegramApi,
+	update: TelegramUpdate,
+	args?: string[]
 ) => Promise<Response>;
 
 export type Commands = Record<string, Command>;
@@ -21,316 +22,316 @@ export type Commands = Record<string, Command>;
 export type Kv = Record<string, KVNamespace> | undefined;
 
 export class Config {
-  bot_name: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  api: any;
-  webhook: Webhook;
-  commands: Record<string, Command>;
-  kv: Kv | undefined;
-  url: URL;
-  handler: Handler | undefined;
+	bot_name: string;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	api: TelegramWebhook | { api: () => [] };
+	webhook: Webhook;
+	commands: Record<string, Command>;
+	kv: Kv | undefined;
+	url: URL;
+	handler: Handler | undefined;
 
-  constructor(config: Partial<Config> = {}) {
-    this.bot_name = config.bot_name || "";
-    this.api = config.api || {};
-    this.webhook = config.webhook || new Webhook(localhost, "", localhost);
-    this.commands = config.commands || {};
-    this.kv = config.kv;
-    this.url = config.url || new URL(localhost);
-    this.handler = config.handler;
-  }
+	constructor(config: Partial<Config> = {}) {
+		this.bot_name = config.bot_name || "";
+		this.api = config.api || { api: () => [] };
+		this.webhook = config.webhook || new Webhook(localhost, "", localhost);
+		this.commands = config.commands || {};
+		this.kv = config.kv;
+		this.url = config.url || new URL(localhost);
+		this.handler = config.handler;
+	}
 }
 
 export const localhost = new URL("http://localhost");
 
 export class WebhookCommands {
-  [key: string]: () => Promise<Response>;
+	[key: string]: () => Promise<Response>;
 }
 
 export type Joke = {
-  error: boolean;
-  category: string;
-  type: string;
-  setup?: string;
-  delivery?: string;
-  joke?: string;
-  flags: {
-    nsfw: boolean;
-    religious: boolean;
-    political: boolean;
-    racist: boolean;
-    sexist: boolean;
-    explicit: boolean;
-  };
-  id: number;
-  safe: boolean;
-  lang: string;
+	error: boolean;
+	category: string;
+	type: string;
+	setup?: string;
+	delivery?: string;
+	joke?: string;
+	flags: {
+		nsfw: boolean;
+		religious: boolean;
+		political: boolean;
+		racist: boolean;
+		sexist: boolean;
+		explicit: boolean;
+	};
+	id: number;
+	safe: boolean;
+	lang: string;
 };
 
 export type Bored = {
-  activity: string;
-  type: string;
-  participants: number;
-  price: number;
-  link: string;
-  key: string;
-  accessibility: 0;
+	activity: string;
+	type: string;
+	participants: number;
+	price: number;
+	link: string;
+	key: string;
+	accessibility: 0;
 };
 
 export type Balance = Record<
-  string,
-  { final_balance: number; n_tx: number; total_received: number }
+	string,
+	{ final_balance: number; n_tx: number; total_received: number }
 >;
 
 export type TelegramFrom = {
-  first_name: string;
-  id: number;
-  is_bot: boolean;
-  language_code: string;
-  username: string;
+	first_name: string;
+	id: number;
+	is_bot: boolean;
+	language_code: string;
+	username: string;
 };
 
 export type TelegramChat = {
-  id: number;
-  type: string;
-  title?: string;
-  username?: string;
-  first_name?: string;
-  last_name?: string;
-  // photo?: TelegramChatPhoto;
-  bio?: string;
-  has_private_forwards: boolean;
-  description?: string;
-  invite_link?: string;
-  pinned_message?: TelegramMessage;
-  // permissions?: TelegramChatPermissions;
-  slow_mode_delay?: number;
-  message_auto_delete_time?: number;
-  has_protected_content?: boolean;
-  sticker_set_name?: string;
-  can_set_sticker_set?: boolean;
-  linked_chat_id?: number;
-  // location?: TelegramChatLocation;
+	id: number;
+	type: string;
+	title?: string;
+	username?: string;
+	first_name?: string;
+	last_name?: string;
+	// photo?: TelegramChatPhoto;
+	bio?: string;
+	has_private_forwards: boolean;
+	description?: string;
+	invite_link?: string;
+	pinned_message?: TelegramMessage;
+	// permissions?: TelegramChatPermissions;
+	slow_mode_delay?: number;
+	message_auto_delete_time?: number;
+	has_protected_content?: boolean;
+	sticker_set_name?: string;
+	can_set_sticker_set?: boolean;
+	linked_chat_id?: number;
+	// location?: TelegramChatLocation;
 };
 
 export type TelegramUser = {
-  id: number;
-  is_bot: boolean;
-  first_name: string;
-  last_name?: string;
-  username?: string;
-  language_code?: string;
-  can_join_groups?: boolean;
-  can_read_all_group_messages?: boolean;
-  supports_inline_queries: boolean;
+	id: number;
+	is_bot: boolean;
+	first_name: string;
+	last_name?: string;
+	username?: string;
+	language_code?: string;
+	can_join_groups?: boolean;
+	can_read_all_group_messages?: boolean;
+	supports_inline_queries: boolean;
 };
 
 export type TelegramMessageEntity = {
-  type: string;
-  offset: number;
-  length: number;
-  url?: string;
-  user?: TelegramUser;
-  language?: string;
+	type: string;
+	offset: number;
+	length: number;
+	url?: string;
+	user?: TelegramUser;
+	language?: string;
 };
 
 export type TelegramPhotoSize = {
-  file_id: string;
-  file_unique_id: string;
-  width: number;
-  height: number;
-  file_size?: number;
+	file_id: string;
+	file_unique_id: string;
+	width: number;
+	height: number;
+	file_size?: number;
 };
 
 export type TelegramMessage = {
-  message_id: number;
-  from: TelegramFrom;
-  sender_chat?: TelegramChat;
-  date: number;
-  chat: TelegramChat;
-  forward_from?: TelegramUser;
-  forward_from_chat?: TelegramChat;
-  forward_from_message_id?: number;
-  forward_signature?: string;
-  forward_sender_name?: string;
-  forward_date?: number;
-  is_automatic_forward?: boolean;
-  reply_to_message?: TelegramMessage;
-  via_bot?: TelegramUser;
-  edit_date?: number;
-  has_protected_content?: boolean;
-  media_group_id?: string;
-  author_signature?: string;
-  text?: string;
-  entities?: TelegramMessageEntity[];
-  // animation?: TelegramAnimation;
-  // audio?: TelegramAudio;
-  // document?: TelegramDocument;
-  photo?: TelegramPhotoSize[];
-  // sticker?: TelegramSticker;
-  // video?: TelegramVideo;
-  // video_note?: TelegramVideoNote;
-  // voice?: TelegramVoice;
-  caption?: string;
-  caption_entities?: TelegramMessageEntity[];
-  // contact?: TelegramContact;
-  // dice?: TelegramDice;
-  // poll?: TelegramPoll;
-  // venue?: TelegramVenue;
-  // location?: TelegramLocation;
-  new_chat_members?: TelegramUser[];
-  left_chat_member?: TelegramUser;
-  new_chat_title?: string;
-  // new_chat_photo?: TelegramPhotoSize[];
-  delete_chat_photo?: boolean;
-  group_chat_created?: boolean;
-  supergroup_chat_created?: boolean;
-  channel_chat_created?: boolean;
-  // message_auto_delete_timer_changed?: TelegramAutoDeleteTimerChanged;
-  migrate_to_chat_id?: number;
-  migrate_from_chat_id?: number;
-  pinned_message?: TelegramMessage;
-  // invoice?: TelegramInvoice;
-  // successful_payment?: TelegramSuccessfulPayment;
-  connected_website?: string;
-  // passport_data?: TelegramPassportData;
-  // proximity_alert_triggered?: TelegramProximityAlertTriggered;
-  // voice_chat_scheduled?: TelegramVoiceChatScheduled;
-  // voice_chat_started?: TelegramVoiceChatStarted;
-  // voice_chat_ended?: TelegramVoiceChatEnded;
-  // voice_chat_participants_invited?: TelegramVoiceChatParticipantsInvited;
-  // reply_markup?: TelegramInlineKeyboardMarkup;
+	message_id: number;
+	from: TelegramFrom;
+	sender_chat?: TelegramChat;
+	date: number;
+	chat: TelegramChat;
+	forward_from?: TelegramUser;
+	forward_from_chat?: TelegramChat;
+	forward_from_message_id?: number;
+	forward_signature?: string;
+	forward_sender_name?: string;
+	forward_date?: number;
+	is_automatic_forward?: boolean;
+	reply_to_message?: TelegramMessage;
+	via_bot?: TelegramUser;
+	edit_date?: number;
+	has_protected_content?: boolean;
+	media_group_id?: string;
+	author_signature?: string;
+	text?: string;
+	entities?: TelegramMessageEntity[];
+	// animation?: TelegramAnimation;
+	// audio?: TelegramAudio;
+	// document?: TelegramDocument;
+	photo?: TelegramPhotoSize[];
+	// sticker?: TelegramSticker;
+	// video?: TelegramVideo;
+	// video_note?: TelegramVideoNote;
+	// voice?: TelegramVoice;
+	caption?: string;
+	caption_entities?: TelegramMessageEntity[];
+	// contact?: TelegramContact;
+	// dice?: TelegramDice;
+	// poll?: TelegramPoll;
+	// venue?: TelegramVenue;
+	// location?: TelegramLocation;
+	new_chat_members?: TelegramUser[];
+	left_chat_member?: TelegramUser;
+	new_chat_title?: string;
+	// new_chat_photo?: TelegramPhotoSize[];
+	delete_chat_photo?: boolean;
+	group_chat_created?: boolean;
+	supergroup_chat_created?: boolean;
+	channel_chat_created?: boolean;
+	// message_auto_delete_timer_changed?: TelegramAutoDeleteTimerChanged;
+	migrate_to_chat_id?: number;
+	migrate_from_chat_id?: number;
+	pinned_message?: TelegramMessage;
+	// invoice?: TelegramInvoice;
+	// successful_payment?: TelegramSuccessfulPayment;
+	connected_website?: string;
+	// passport_data?: TelegramPassportData;
+	// proximity_alert_triggered?: TelegramProximityAlertTriggered;
+	// voice_chat_scheduled?: TelegramVoiceChatScheduled;
+	// voice_chat_started?: TelegramVoiceChatStarted;
+	// voice_chat_ended?: TelegramVoiceChatEnded;
+	// voice_chat_participants_invited?: TelegramVoiceChatParticipantsInvited;
+	// reply_markup?: TelegramInlineKeyboardMarkup;
 };
 
 export type TelegramInputMessageContent = {
-  message_text: string;
-  parse_mode: string;
+	message_text: string;
+	parse_mode: string;
 };
 
 export type TelegramInlineQuery = {
-  chat_type: "sender" | "private" | "group" | "supergroup" | "channel";
-  from: TelegramFrom;
-  id: number;
-  offset: string;
-  query: string;
+	chat_type: "sender" | "private" | "group" | "supergroup" | "channel";
+	from: TelegramFrom;
+	id: number;
+	offset: string;
+	query: string;
 };
 
 export class Update {
-  [key: string]: unknown;
+	[key: string]: unknown;
 }
 
 export class TelegramUpdate extends Update {
-  update_id: number;
-  message?: TelegramMessage;
-  edited_message?: TelegramMessage;
-  channel_post?: TelegramMessage;
-  edited_channel_post?: TelegramMessage;
-  inline_query?: TelegramInlineQuery;
-  // chosen_inline_result?: TelegramChosenInlineResult;
-  // callback_query?: TelegramCallbackQuery;
-  // shipping_query?: TelegramShippingQuery;
-  // pre_checkout_query?: TelegramPreCheckoutQuery;
-  // poll?: TelegramPoll;
-  // poll_answer?: TelegramPollAnswer;
-  // my_chat_member?: TelegramChatMemberUpdated;
-  // chat_member?: TelegramChatMemberUpdated;
-  // chat_join_request: TelegramChatJoinRequest;
-  constructor(update: PartialTelegramUpdate) {
-    super();
-    this.update_id = update.update_id || 0;
-    this.message = update.message;
-    this.edited_message = update.edited_message;
-    this.channel_post = update.channel_post;
-    this.edited_channel_post = update.edited_channel_post;
-    this.inline_query = update.inline_query;
-    // chosen_inline_result = update.chosen_inline_result;
-    // callback_query = update.callback_query;
-    // shipping_query = update.shipping_query;
-    // pre_checkout_query = update.pre_checkout_query;
-    // poll = update.poll;
-    // poll_answer = update.poll_answer;
-    // my_chat_member = update.my_chat_member;
-    // chat_member = update.chat_member;
-    // chat_join_request = update.chat_join_request;
-  }
+	update_id: number;
+	message?: TelegramMessage;
+	edited_message?: TelegramMessage;
+	channel_post?: TelegramMessage;
+	edited_channel_post?: TelegramMessage;
+	inline_query?: TelegramInlineQuery;
+	// chosen_inline_result?: TelegramChosenInlineResult;
+	// callback_query?: TelegramCallbackQuery;
+	// shipping_query?: TelegramShippingQuery;
+	// pre_checkout_query?: TelegramPreCheckoutQuery;
+	// poll?: TelegramPoll;
+	// poll_answer?: TelegramPollAnswer;
+	// my_chat_member?: TelegramChatMemberUpdated;
+	// chat_member?: TelegramChatMemberUpdated;
+	// chat_join_request: TelegramChatJoinRequest;
+	constructor(update: PartialTelegramUpdate) {
+		super();
+		this.update_id = update.update_id || 0;
+		this.message = update.message;
+		this.edited_message = update.edited_message;
+		this.channel_post = update.channel_post;
+		this.edited_channel_post = update.edited_channel_post;
+		this.inline_query = update.inline_query;
+		// chosen_inline_result = update.chosen_inline_result;
+		// callback_query = update.callback_query;
+		// shipping_query = update.shipping_query;
+		// pre_checkout_query = update.pre_checkout_query;
+		// poll = update.poll;
+		// poll_answer = update.poll_answer;
+		// my_chat_member = update.my_chat_member;
+		// chat_member = update.chat_member;
+		// chat_join_request = update.chat_join_request;
+	}
 }
 
 export type PartialTelegramUpdate = {
-  update_id?: number;
-  message?: TelegramMessage;
-  edited_message?: TelegramMessage;
-  channel_post?: TelegramMessage;
-  edited_channel_post?: TelegramMessage;
-  inline_query?: TelegramInlineQuery;
+	update_id?: number;
+	message?: TelegramMessage;
+	edited_message?: TelegramMessage;
+	channel_post?: TelegramMessage;
+	edited_channel_post?: TelegramMessage;
+	inline_query?: TelegramInlineQuery;
 };
 
 export type TelegramInlineQueryType =
-  | "article"
-  | "photo"
-  | "gif"
-  | "mpeg4_gif"
-  | "video"
-  | "audio"
-  | "voice"
-  | "document"
-  | "location"
-  | "venue"
-  | "contact"
-  | "game"
-  | "sticker";
+	| "article"
+	| "photo"
+	| "gif"
+	| "mpeg4_gif"
+	| "video"
+	| "audio"
+	| "voice"
+	| "document"
+	| "location"
+	| "venue"
+	| "contact"
+	| "game"
+	| "sticker";
 
 export class TelegramInlineQueryResult {
-  type: TelegramInlineQueryType;
-  id: string;
-  constructor(type: TelegramInlineQueryType) {
-    this.type = type;
-    this.id = crypto.randomUUID();
-  }
+	type: TelegramInlineQueryType;
+	id: string;
+	constructor(type: TelegramInlineQueryType) {
+		this.type = type;
+		this.id = crypto.randomUUID();
+	}
 }
 
 export class TelegramInlineQueryResultPhoto extends TelegramInlineQueryResult {
-  photo_url: string; // must be a jpg
-  thumb_url: string;
-  photo_width?: number;
-  photo_height?: number;
-  title?: string;
-  description?: string;
-  caption?: string;
-  parse_mode?: string;
-  caption_entities?: string;
-  // reply_markup?: TelegramInlineKeyboardMarkup;
-  // input_message_content?: TelegramInputMessageContent;
-  constructor(photo: string) {
-    super("photo");
-    this.photo_url = photo;
-    this.thumb_url = photo;
-  }
+	photo_url: string; // must be a jpg
+	thumb_url: string;
+	photo_width?: number;
+	photo_height?: number;
+	title?: string;
+	description?: string;
+	caption?: string;
+	parse_mode?: string;
+	caption_entities?: string;
+	// reply_markup?: TelegramInlineKeyboardMarkup;
+	// input_message_content?: TelegramInputMessageContent;
+	constructor(photo: string) {
+		super("photo");
+		this.photo_url = photo;
+		this.thumb_url = photo;
+	}
 }
 
 export class TelegramInlineQueryResultArticle extends TelegramInlineQueryResult {
-  title: string;
-  input_message_content: TelegramInputMessageContent;
-  thumb_url: string;
-  constructor(
-    content: string,
-    title = content,
-    parse_mode = "",
-    thumb_url = ""
-  ) {
-    super("article");
-    this.title = title;
-    this.input_message_content = {
-      message_text: content.toString(),
-      parse_mode,
-    };
-    this.thumb_url = thumb_url;
-  }
+	title: string;
+	input_message_content: TelegramInputMessageContent;
+	thumb_url: string;
+	constructor(
+		content: string,
+		title = content,
+		parse_mode = "",
+		thumb_url = ""
+	) {
+		super("article");
+		this.title = title;
+		this.input_message_content = {
+			message_text: content.toString(),
+			parse_mode,
+		};
+		this.thumb_url = thumb_url;
+	}
 }
 
 export type DDGQueryResponse = {
-  AbstractSource: string;
-  AbstractURL: string;
-  Redirect: string;
-  Image: string;
-  RelatedTopics: { Icon: { URL: string } }[];
+	AbstractSource: string;
+	AbstractURL: string;
+	Redirect: string;
+	Image: string;
+	RelatedTopics: { Icon: { URL: string } }[];
 };
